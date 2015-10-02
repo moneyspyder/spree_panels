99dd5ae23933769af8e916ec0ace31e91a9f2d87
Christopher Reeve <cjreeve@gmail.com>
2015-10-02 15:30:00 +0100

    remove 'foreign_key = true' from spree_panel_group_allocations migration

--- db/migrate/20151001151346_create_spree_panel_group_allocations.rb
+++ db/migrate/20151001151346_create_spree_panel_group_allocations.rb
@@ -1,8 +1,8 @@
 class CreateSpreePanelGroupAllocations < ActiveRecord::Migration
   def change
     create_table :spree_panel_group_allocations do |t|
-      t.references :panel, index: true, foreign_key: true
-      t.references :panel_group, index: true, foreign_key: true
+      t.references :panel, index: true
+      t.references :panel_group, index: true
       t.integer :position, index: true
     end
   end
