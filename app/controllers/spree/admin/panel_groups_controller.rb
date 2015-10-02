6b92204531251953fed973607c825ccf2fa5c159
Christopher Reeve <cjreeve@gmail.com>
2015-10-02 19:43:04 +0100

    add url field to panel form and redirect to edit page after save

--- app/controllers/spree/admin/panel_groups_controller.rb
+++ app/controllers/spree/admin/panel_groups_controller.rb
@@ -2,17 +2,16 @@ module Spree
   module Admin
     class PanelGroupsController < ResourceController
 
-      def update
-        if  @object.update_attributes(panel_group_params)
-          redirect_to location_after_save
-        else
-          respond_with(@object)
-        end
-      end
+      private
 
       def panel_group_params
         params.require(:panel_group).permit(:name, :identifier, :position, panel_ids: [])
       end
+
+      def location_after_save
+        url_for([:edit, :admin, @panel_group])
+      end
+
     end
   end
 end
--- app/controllers/spree/admin/panels_controller.rb
+++ app/controllers/spree/admin/panels_controller.rb
@@ -1,6 +1,13 @@
 module Spree
   module Admin
     class PanelsController < ResourceController
+
+      private
+
+      def location_after_save
+        url_for([:edit, :admin, @panel])
+      end
+
     end
   end
 end
