525777a7f74009bf6988b93f1dd1d529e7b96b8c
Christopher Reeve <cjreeve@gmail.com>
2015-10-07 18:03:56 +0100

    fix to get current panels for panel group and add @panels to panel_group controller

--- app/controllers/spree/admin/panel_groups_controller.rb
+++ app/controllers/spree/admin/panel_groups_controller.rb
@@ -1,8 +1,13 @@
 module Spree
   module Admin
     class PanelGroupsController < ResourceController
+      before_filter :load_panels, only: [:new, :edit]
 
       private
+
+      def load_panels
+        @panels = Spree::Panel.order(:name)
+      end
 
       def panel_group_params
         params.require(:panel_group).permit(:name, :identifier, :position, panel_ids: [])
